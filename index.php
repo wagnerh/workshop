294843cc3648d2f2023ff56d1fae58b343199e44
wagnerh <chatoo@gmail.com>
2018-04-12 19:03:33 +0000

    Conexao a banco

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
-<html>
- <head>
-  <title>Teste PHP</title>
- </head>
- <body>
- <?php echo "<p>Olá Mundo, Bem vindo a Maceió! V.1.0</p>"; ?>
- </body>
-</html>
+<?php
+echo "<h1>Openshift Workshop v2.0</h1> ";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br><hr>";
+echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<h3>" . $row["nome"] . "</h3>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
